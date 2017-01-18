3b83fcd3394e1ce4b73461adce77cae9797aa6f8
Daniel Sousa <sousa.danielg@gmail.com>
2017-01-17 18:02:19 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	context: __dirname + '/app',
+	entry: './index.jsx',
+	output: {
+		path: 'public',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx$/,
+				exclude: '/node_modules/',
+				loader: "babel-loader"
+			},
+			{
+				test: /\.scss$/,
+				loader: "style-loader!css-loader!sass-loader"
+			}
+		]
+	},
+	// plugins: [
+	// 	new webpack.optimize.UglifyJsPlugin({minimize: true, compress: {warnings: false}})
+	// ]
+}
